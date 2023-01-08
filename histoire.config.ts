ed4b65dd40c2c14f1ca245a5fa0593684cb90494
Nicolas Hedger <nicolas@hedger.ch>
2023-01-08 23:28:25 +0100

    fix: add missing base path

--- histoire.config.ts
+++ histoire.config.ts
@@ -4,4 +4,7 @@ import { HstVue } from '@histoire/plugin-vue';
 export default defineConfig({
     setupFile: 'src/histoire.ts',
     plugins: [HstVue()],
+    vite: {
+        base: '/components',
+    },
 });
